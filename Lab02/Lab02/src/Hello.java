8b634850e00dd4c8ed1faf55e3d680a0faf0b785
isaiflores <isaiflores@isais-mbp>
2022-06-10 20:28:46 -0700

    lab02

--- Lab02/Lab02/src/Hello.java
+++ Lab02/Lab02/src/Hello.java
@@ -12,7 +12,6 @@ public class Hello {
         System.out.println("SacStateID - This line was added by First name Last name.");
         System.out.println("SacStateID - This line was added by First name Last name.");
         System.out.println("SacStateID - This line was added by First name Last name.");
-        System.out.println("SacStateID - This line was added by First name Last name.");
         System.out.println("218714484 - This line was added by Isai Flores.");
 
 }
